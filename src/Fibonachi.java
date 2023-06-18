27fc985798da65345759bd8c2e5f06e42aa5ec23
SergiyPidoprygora <134638933+SergiyPidoprygora@users.noreply.github.com>
2023-06-18 18:55:32 +0300

    Update Fibonachi.java

--- src/Fibonachi.java
+++ src/Fibonachi.java
@@ -3,15 +3,15 @@ import jdk.jshell.spi.ExecutionControl;
 public class Fibonachi {
     public static void main(String[] args) {
         //4.Вивести числа фібоначчі до 100 (значення самого числа не повинно перевищувати 100)
-        int number1 = 0;
-        int number2 = 1;
-        int number3 = 0;
-        System.out.print(number1 + " " + number2 + " ");
-        for (int i = 0; i <= 100; i++) {
-            number3 = number1 + number2;
-            System.out.print(number2 + " ");
-            number1 = number2;
-            number2 = number3;
+        int n0 = 1;
+        int n1 = 1;
+        int n2 = 0;
+        System.out.print(n0 + " " + n1 + " ");
+        for (int i = 3; i <= 11; i++) {
+            n2 = n0 + n1;
+            System.out.print(n2+ " ");
+            n0 = n1;
+            n1 = n2;
 
 
         }
